c13a810d3b6d2931d5942c6f6d1368b13d9151ed
Anton Eliasson <devel@antoneliasson.se>
2012-12-18 14:52:03 +0100

    Ändrat testraden i DB och synkat initializedb med tests

--- writers_choice/scripts/initializedb.py
+++ writers_choice/scripts/initializedb.py
@@ -36,5 +36,5 @@ def main(argv=sys.argv):
         from datetime import date
         article = Article(title='Testsida', body='''Ett stycke.
 
-Nästa stycke.''', published=date(2012, 12, 17))
+Ett *stycke* till.''', published=date(2012, 1, 1))
         DBSession.add(article)
--- writers_choice/tests.py
+++ writers_choice/tests.py
@@ -21,7 +21,7 @@ class TestMyView(unittest.TestCase):
             from datetime import date
             article = Article(title='Testsida', body='''Ett stycke.
 
-Ett *stycke* till.''', published=date(2012, 12, 17))
+Ett *stycke* till.''', published=date(2012, 1, 1))
             DBSession.add(article)
 
     def tearDown(self):
