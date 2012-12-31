0d7db6e5f8842ef792b7ecde5f7a17c1aa636fab
Anton Eliasson <devel@antoneliasson.se>
2012-12-31 01:03:35 +0100

    Klar med task 5.1 och 5.3.
    * Ny vy edit_article som är mycket lik add_article, men som först kikar i DB efter en befintlig artikel.
      - submit_url genereras i vyn istället för i mallen. add_article kommer nog ändras för att göra likadant.
      - Ingen felhantering i vanlig ordning.
    * Ny rutt till edit_article efter add_article men före view_article.

--- writers_choice/views.py
+++ writers_choice/views.py
@@ -58,7 +58,25 @@ def add_article(request):
 
         return HTTPFound(location = request.route_url('view_article', id=article.id))
     return {}
-        
+
+@view_config(route_name='edit_article', renderer='templates/edit_article.pt')
+def edit_article(request):
+    id = request.matchdict['id']
+    article = DBSession.query(Article).filter_by(id=id).one()
+
+    if 'title' in request.params:
+        from pyramid.httpexceptions import HTTPFound
+        title = request.params['title']
+        body = request.params['body']
+        article.title = title
+        article.body = body
+        DBSession.add(article)
+
+        return HTTPFound(location = request.route_url('view_article', id=article.id))
+
+    submit_url = request.route_url('edit_article', id=id)
+    return {'title' : article.title, 'body' : article.body, 'submit_url' : submit_url}
+
 conn_err_msg = """\
 (Tabellen finns inte)
 
