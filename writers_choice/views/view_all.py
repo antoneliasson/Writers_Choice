ccf646ccb3a11dfb69487a1d7c2434391167e198
Anton Eliasson <devel@antoneliasson.se>
2013-02-23 22:12:46 +0100

    Klar med Story 6
    Testfallet test_view_all_two_articles uppdaterat för att påvisa ändringen
    som krävs. I view_all är queryn försedd med en order_by som sorterar efter
    publiceringsdatum i fallande ordning.

--- writers_choice/views/view_all.py
+++ writers_choice/views/view_all.py
@@ -12,7 +12,7 @@ from . import format_article
 @view_config(route_name='view_all', renderer='writers_choice:templates/view_all.pt')
 def view_all(request):
     try:
-        articles = DBSession.query(Article)
+        articles = DBSession.query(Article).order_by(Article.published.desc())
     except DBAPIError:
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
 
