7f0e2a5981e50a231a4c25b0ca6005a36b07f478
Anton Eliasson <devel@antoneliasson.se>
2014-01-12 23:07:16 +0100

    Städning i test_models

--- writers_choice/tests/test_models.py
+++ writers_choice/tests/test_models.py
@@ -1,14 +1,16 @@
 import unittest
 import datetime
 
-from writers_choice.models import Article, Page#, Editor
+from writers_choice.models import Article, Page
 
 class ArticleTests(unittest.TestCase):
     def test_article(self):
-        article = Article('Testsida',
-                          body='Ett stycke.\n\nEtt *stycke* till.\n',
-                          is_published=True,
-                          date_published=datetime.date(2012, 1, 1))
+        article = Article(
+            'Testsida',
+            body='Ett stycke.\n\nEtt *stycke* till.\n',
+            is_published=True,
+            date_published=datetime.date(2012, 1, 1)
+        )
         self.assertEqual(article.title, 'Testsida')
         self.assertEqual(article.body, 'Ett stycke.\n\nEtt *stycke* till.')
         self.assertEqual(article.is_published, True)
@@ -32,10 +34,3 @@ class PageTests(unittest.TestCase):
         page.title = 'About Me'
         self.assertEqual(page.title, 'About Me')
         self.assertEqual(page.slug, 'about')
-
-# class EditorTests(unittest.TestCase):
-#     def test_editor(self):
-#         editor = Editor(email='editor@example.com',
-#                         joined=datetime.date(2013, 1, 1))
-#         self.assertEqual(editor.email, 'editor@example.com')
-#         self.assertEqual(editor.joined, datetime.date(2013, 1, 1))
